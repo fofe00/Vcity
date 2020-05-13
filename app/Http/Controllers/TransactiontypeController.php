a1d70d36a038fd32d1d7ab0f9c4962411a43153f
lonfo fofe <b.fofe@yahoo.com>
2020-05-13 15:32:26 +0100

    init project

---
+++ app/Http/Controllers/TransactiontypeController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TransactiontypeController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/TypecompteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\typecompte;
+use Illuminate\Http\Request;
+
+class TypecompteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\typecompte  $typecompte
+     * @return \Illuminate\Http\Response
+     */
+    public function show(typecompte $typecompte)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\typecompte  $typecompte
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(typecompte $typecompte)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\typecompte  $typecompte
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, typecompte $typecompte)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\typecompte  $typecompte
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(typecompte $typecompte)
+    {
+        //
+    }
+}
