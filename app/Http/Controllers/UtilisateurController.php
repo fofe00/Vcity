56a81c4d3625d5491863230de25a08624b6ba83b
LONFO FOFE <b.fofe@yahoo.com>
2020-05-14 13:12:36 +0100

    users fuction base

--- app/Http/Controllers/UtilisateurController.php
+++ app/Http/Controllers/UtilisateurController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class UtilisateurController extends Controller
 {
-    //
+    public function login(){
+      return \view('auth.login');
+    }
+
+    public function tLogin(){
+      //login treatment
+    }
+
+    public function register(){
+      return view('auth.register');
+    }
+    public function tRegister(){
+      // register treatment
+    }
 }
